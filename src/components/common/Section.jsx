91f5365656dd017309c5d7acce24f8e4ab855473
Nishant <nishantsingh95340@gmail.com>
2023-09-20 16:06:07 +0530

    setup layout

---
+++ src/components/common/Section.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Section = ({ children }) => {
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-1">{children}</div>
+    </div>
+  );
+};
+
+export default Section;
